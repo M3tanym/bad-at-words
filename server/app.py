6e64bf2465beb6781cd470c3152949b0f8bfc058
tim4tech <timothy.ford@mines.sdsmt.edu>
2019-12-30 23:01:28 -0700

    maybe a response

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 from starlette.websockets import WebSocket
 
+import json
+
 
 VERSION = 0.1
 
@@ -22,6 +24,25 @@ async def version():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+
+    words = ["barry", "juice", "nuts"]
+    wordDict = Convert(words)
+
+    response = { 
+        "type" : "update", 
+        "words" : wordDict
+    }
+
+    r = json.dumps(response)
+    await websocket.send_text(str(r))
+
+
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")
+
+
+
+def Convert(lst): 
+    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)} 
+    return res_dct 
