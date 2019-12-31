7e214e05c7cd0a317787aca42c0cf0f4fc7c5b61
tim4tech <timothy.ford@mines.sdsmt.edu>
2019-12-31 00:01:51 -0700

    a bunch of stuff that does a bunch of stuff

--- server/app.py
+++ server/app.py
@@ -6,6 +6,9 @@ from starlette.websockets import WebSocket # host websockets
 
 import json
 
+from player import Player
+from logic import handleTouch
+
 
 VERSION = 0.1
 
@@ -14,6 +17,10 @@ app = FastAPI()
 app.mount("/client", StaticFiles(directory="../client"), name="client")
 
 words = []
+roundWords = []
+players = []
+
+playerIdCount = 0
 
 def get_sample():
     return sample(words, 25)
@@ -23,6 +30,9 @@ async def startup_event():
     global words
     with open("words.txt") as word_file:
         words = word_file.read().splitlines()
+
+    global roundWords 
+    roundWords = get_sample()
 
 @app.get("/")
 async def get():
@@ -37,23 +47,59 @@ async def version():
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
-    wordDict = get_sample()
+    # new player gets created on new websocket
+    global playerIdCount, players
+    players.append(Player("name", playerIdCount, "red"))
+    playerIdCount += 1
 
+    # send random sample of words on startup
+    global roundWords
+    wordDict = roundWords
     response = {
         "type" : "update",
         "words" : wordDict
     }
-
     r = json.dumps(response)
     await websocket.send_text(str(r))
 
-
+    # Loop to handle all subsequent requets
     while True:
+        # receive json message from client
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+
+        r = handleMessage(data)
+
+        # if reponse generated, send response
+        if r is not None:
+            await websocket.send_text(r)
 
 
+def handleMessage(data):
+    """
+    handles different json messages from websocket
+    
+    Arguments:
+        data {json} -- json message from client
+    """
+    r = json.loads(data)
+    rType = r["type"]
 
-def Convert(lst):
-    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-    return res_dct
+    if rType == "touch":
+        """
+        {
+            type : "touch",
+            word : word,
+            player : id,
+        }
+        """
+        rWord = str(r["word"])
+        # player id
+        rPlayer = int(r["player"])
+
+        # HANDLE THE LOGIC HERE
+        # handleTouch(rWord, rPlayer)
+
+        # return reponse to client?
+        return None
+    else:
+        print("Case may not be handled yet")
